98c83c2e12571d357fdef97235559ca463c51c7c
RenatAmutov <130355064+RenatAmutov@users.noreply.github.com>
2023-05-01 17:32:48 +0600

    4

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = (a%100 /10);
+
+Console.WriteLine($"Средняя чифра даного числа = {b}");
